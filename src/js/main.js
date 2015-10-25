4373c0788a8226407ee03bac3efa072b0dbc528e
David Bernheisel <david@bernheisel.com>
2015-10-25 01:50:51 -0400

    Fix routes attempt #3

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,36 @@
-
-
-
-
 ;(function(){ //iife for angularjs
-
-
-  angular.module ('catsoverflow',['ngRoute'],
+  angular.module('catsoverflow',['ngRoute'],
     function($routeProvider){
         $routeProvider
         .when('/', {
-          templateUrl: ''
+          templateUrl: 'partials/home.html',
+          controller: 'MainCtrl'
         })
         .when('/login', {
           templateUrl: 'partials/signup.html',
           controller: 'signup-controller'
         })
         .when('/home', {
-          templateUrl: 'partials/home.html'
+          templateUrl: 'partials/home.html',
+
         })
         .when('/ask-question', {
-          templateUrl: 'partials/qa.html'
+          templateUrl: 'partials/qa.html',
+
         });
     })
 
-    .run([ '$rootScope','$http', function($scope, $http){
-      $http.get("https://cats-overflow.herokuapp.com/questions.json") // will work running html in Browser Sync
-        .then(function(response){
-          $scope.questions = response.data;
-        });
+    // .run([ '$rootScope','$http', function($scope, $http){
 
-    }])
+    // }])
+
+    .controller("MainCtrl", function($scope, $http){
+      $http.get("https://cats-overflow.herokuapp.com/questions.json")
+      .then(function(response){
+        $scope.questions = response.data;
+      });
+    })
+
     .controller("signup-controller", function($scope, $http){
       $scope.signupData = {
         display_name: "",
@@ -37,19 +38,12 @@
         full_name: "",
         password: ""
       }
-
       $scope.processSignUp = function(){
         $http.post("http://cats-overflow.herokuapp.com/users.json", $scope.signupData)
-        .then(function (response){
-          console.log(response);
+        .then(function(data){
+          console.log(data);
         });
-        return;
       };
 
     });
-
-
-
-
-
 })();
